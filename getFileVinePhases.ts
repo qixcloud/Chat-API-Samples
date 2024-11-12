b421a3f7aa6609d2f36065e6101d18c9aeb73844
Rostyslav <roctik4x@gmail.com>
2024-11-12 18:57:02 +0200

    get FileVine Phases

---
+++ getFileVinePhases.ts
@@ -0,0 +1,24 @@
+{
+  interface IPhase {
+    phase: string,
+    title: string,
+    description: string,
+    order: number,
+    fileVineId: string,
+  }
+
+  const jwtToken:string = '[token]';
+
+  fetch('https://api.qix.cloud/phaseFileVine', {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + jwtToken,
+    },
+  }).then((res) => {
+    return res.json();
+  }).then((res:IPhase[]) => {
+    
+    const phaseList:IPhase[] = res; // list of phases
+
+  })
+}
